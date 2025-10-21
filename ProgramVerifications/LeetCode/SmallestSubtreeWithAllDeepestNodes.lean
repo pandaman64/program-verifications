c1a28d49ded109ff8eff9e2ebc0300dd3ce1df11
pandaman64 <kointosudesuyo@infoseek.jp>
2025-10-21 23:07:10 +0900

    Subtree definition based on accessors

--- ProgramVerifications/LeetCode/SmallestSubtreeWithAllDeepestNodes.lean
+++ ProgramVerifications/LeetCode/SmallestSubtreeWithAllDeepestNodes.lean
@@ -42,124 +42,57 @@ where
       | .eq => (root, leftHeight + 1)
       | .gt => (left', leftHeight + 1)
 
--- inductive IsSubtreeOf (tree : Tree α) : Tree α → Prop where
---   | refl : IsSubtreeOf tree tree
---   | left {value left right} (h : IsSubtreeOf tree left) : IsSubtreeOf tree (.node value left right)
---   | right {value left right} (h : IsSubtreeOf tree right) : IsSubtreeOf tree (.node value left right)
-
 namespace Tree
 
-inductive SubtreeAt (root : Tree α) : Tree α → Nat → Prop where
-  | refl : Tree.SubtreeAt root root 0
-  | left {value left right depth} (h : Tree.SubtreeAt root (.node value left right) depth) : Tree.SubtreeAt root left (depth + 1)
-  | right {value left right depth} (h : Tree.SubtreeAt root (.node value left right) depth) : Tree.SubtreeAt root right (depth + 1)
+inductive TreeAccess where
+  | left
+  | right
 
-@[grind]
-theorem SubtreeAt.ofLeft {value : α} {left right tree depth} (h : Tree.SubtreeAt left tree depth) : Tree.SubtreeAt (Tree.node value left right) tree (depth + 1) := by
-  induction h with
-  | refl => exact .left .refl
-  | left _ ih => exact .left ih
-  | right _ ih => exact .right ih
-
-@[grind]
-theorem SubtreeAt.ofRight {value : α} {left right tree depth} (h : Tree.SubtreeAt right tree depth) : Tree.SubtreeAt (Tree.node value left right) tree (depth + 1) := by
-  induction h with
-  | refl => exact .right .refl
-  | left _ ih => exact .left ih
-  | right _ ih => exact .right ih
-
-theorem height_plus_depth_le_total_height (h : Tree.SubtreeAt root tree depth) :
-  tree.height + depth ≤ root.height := by
-  induction h <;> grind [Tree.height]
-
-@[grind]
-def IsDeepestSubtreeOf (tree : Tree α) (root : Tree α) : Prop :=
-  ∃ depth, root.SubtreeAt tree depth ∧ ∀ tree' depth', root.SubtreeAt tree' depth' → depth' ≤ depth
+def get' (as : List TreeAccess) (tree : Tree α) : Tree α :=
+  match as with
+  | [] => tree
+  | .left :: as => get' as tree.left
+  | .right :: as => get' as tree.right
 
 @[simp, grind]
-theorem left_nil_of_isDeepestSubtreeOf {tree root : Tree α} (h : tree.IsDeepestSubtreeOf root) : tree.left = .nil := by
-  cases tree with
-  | nil => grind [Tree.left]
-  | node value left right =>
-    obtain ⟨depth, hd, hmax⟩ := h
-    cases hd with
-    | refl =>
-      have := hmax left 1 (.left .refl)
-      grind
-    | @left value' _ right' depth' hd =>
-      have := hmax left (depth' + 2) (.left (.left hd))
-      grind
-    | @right value' _ left' depth' hd =>
-      have := hmax left (depth' + 2) (.left (.right hd))
-      grind
+theorem get'_nil (tree : Tree α) : get' [] tree = tree := rfl
 
 @[simp, grind]
-theorem right_nil_of_isDeepestSubtreeOf {tree root : Tree α} (h : tree.IsDeepestSubtreeOf root) : tree.right = .nil := by
-  cases tree with
-  | nil => grind [Tree.right]
-  | node value left right =>
-    obtain ⟨depth, hd, hmax⟩ := h
-    cases hd with
-    | refl =>
-      have := hmax right 1 (.right .refl)
-      grind
-    | @left value' _ right' depth' hd =>
-      have := hmax right (depth' + 2) (.right (.left hd))
-      grind
-    | @right value' _ left' depth' hd =>
-      have := hmax right (depth' + 2) (.right (.right hd))
-      grind
+theorem get'_cons_left (as : List TreeAccess) (tree : Tree α) : get' (.left :: as) tree = get' as tree.left := rfl
+
+@[simp, grind]
+theorem get'_cons_right (as : List TreeAccess) (tree : Tree α) : get' (.right :: as) tree = get' as tree.right := rfl
+
+@[simp, grind]
+theorem get'_nil_tree (as : List TreeAccess) : get' as (.nil : Tree α) = .nil := by
+  induction as with
+  | nil => rfl
+  | cons a as ih => cases a <;> simp [get', ih]
+
+@[simp, grind]
+theorem get'_append (as₁ as₂ : List TreeAccess) (tree : Tree α) : get' (as₁ ++ as₂) tree = get' as₂ (get' as₁ tree) := by
+  induction as₁ generalizing tree with
+  | nil => simp
+  | cons a as₁ ih =>
+    cases a with
+    | left => simp [ih]
+    | right => simp [ih]
 
 @[grind]
-theorem eq_nil_or_singleton_of_isDeepestSubtreeOf {tree root : Tree α} (h : tree.IsDeepestSubtreeOf root) :
-  tree = .nil ∨ ∃ value, tree = .node value .nil .nil := by
-  match eq : tree with
-  | .nil => exact .inl rfl
-  | .node value left right =>
-    have : tree.left = left := by simp [Tree.left, eq]
-    have : tree.right = right := by simp [Tree.right, eq]
-    grind
+def IsDeepestAccess (root : Tree α) (as : List TreeAccess) : Prop :=
+  root.get' as ≠ .nil ∧ ∀ as', root.get' as' ≠ .nil → as'.length ≤ as.length
 
-@[grind]
-def deepestSubtree (root : Tree α) : Tree α :=
-  match root with
-  | .nil => .nil
-  | .node _value left right =>
-    if left.height ≤ right.height then
-      deepestSubtree right
-    else
-      deepestSubtree left
-
-theorem subtreeAt_deepestSubtree_height (root : Tree α) : root.SubtreeAt (deepestSubtree root) root.height := by
-  induction root with
-  | nil => exact .refl
-  | node _value left right ihLeft ihRight =>
-    if h : left.height ≤ right.height then
-      simpa [deepestSubtree, h] using .ofRight ihRight
-    else
-      have h' : right.height ≤ left.height := by grind
-      simpa [deepestSubtree, h, h'] using .ofLeft ihLeft
-
-theorem isDeepestSubtreeOf_deepestSubtree (root : Tree α) : (deepestSubtree root).IsDeepestSubtreeOf root := by
-  refine ⟨root.height, subtreeAt_deepestSubtree_height root, ?_⟩
-  intro tree depth h
-  exact Nat.le_trans (by grind) (height_plus_depth_le_total_height h)
-
-theorem subtreeAt_height_of_isDeepestSubtreeOf {tree root : Tree α} (h : tree.IsDeepestSubtreeOf root) :
-  root.SubtreeAt tree root.height := by
-  obtain ⟨depth, hd, hmax⟩ := h
-  have ge : root.height ≤ depth := hmax (deepestSubtree root) root.height (subtreeAt_deepestSubtree_height root)
-  have le : depth ≤ root.height := Nat.le_trans (by grind) (height_plus_depth_le_total_height hd)
+theorem length_eq_of_IsDeepestAccess {root : Tree α} {as₁ as₂ : List TreeAccess}
+  (h₁ : root.IsDeepestAccess as₁) (h₂ : root.IsDeepestAccess as₂) :
+  as₁.length = as₂.length := by
   grind
 
-theorem isDeepestSubtreeOf_of_subtreeAt_height {tree root : Tree α} (h : root.SubtreeAt tree root.height) :
-  tree.IsDeepestSubtreeOf root := by
-  refine ⟨root.height, h, ?_⟩
-  intro tree depth h
-  exact Nat.le_trans (by grind) (height_plus_depth_le_total_height h)
+@[grind]
+def IsAncestorOfDeepestAccesses (root : Tree α) (as : List TreeAccess) : Prop :=
+  ∀ as', root.IsDeepestAccess as' → as <+: as'
 
-@[simp, grind]
-theorem isDeepestSubtreeOf_iff_subtreeAt_height {tree root : Tree α} :
-  tree.IsDeepestSubtreeOf root ↔ root.SubtreeAt tree root.height := ⟨subtreeAt_height_of_isDeepestSubtreeOf, isDeepestSubtreeOf_of_subtreeAt_height⟩
+@[grind]
+def IsLongestAncestorOfDeepestAccesses (root : Tree α) (as : List TreeAccess) : Prop :=
+  ∀ as', root.IsAncestorOfDeepestAccesses as' → as'.length ≤ as.length
 
 end Tree
